14e2354bde0c2ff2d73c53e0b0793ae9df6cd016
Dev-Everly <everlysantos98@gmail.com>
2026-06-29 16:42:18 -0300

    :sparkles: Classe Carro eletrico herança

--- src/atividade_02/Carro.java
+++ src/atividade_02/Carro.java
@@ -2,4 +2,42 @@ package atividade_02;
 
 public class Carro {
 
+	private String modelo;
+	private int velocidade;
+	
+	
+	
+	public Carro(String modelo, int velocidade) {
+		super();
+		this.modelo = modelo;
+		this.velocidade = velocidade;
+	}
+	
+	
+	
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getVelocidade() {
+		return velocidade;
+	}
+	public void setVelocidade(int velocidade) {
+		this.velocidade = velocidade;
+	}
+	
+	public void acelerar(int valor) {
+		 
+		velocidade  += valor;
+		System.out.println("Acelerar " +  valor +" km/h");
+	}
+	
+	public void visualizar() {
+		System.out.println("""
+				Modelo : 
+				Velocidade Atual : 
+				""".formatted(modelo, velocidade));
+	}
 }
--- src/atividade_02/CarroEletrico.java
+++ src/atividade_02/CarroEletrico.java
@@ -1,5 +1,37 @@
 package atividade_02;
 
-public class CarroEletrico {
+public class CarroEletrico extends Carro{
 
+	private int nivelBateria;
+	private static final int CONSUMO_POR_ACELERACAO = 2;
+
+	public CarroEletrico(String modelo, int velocidade, int nivelBateria) {
+		super(modelo, velocidade);
+		this.nivelBateria = nivelBateria;
+	}
+	
+	public int getNivelBateria() {
+		return nivelBateria;
+	}
+
+
+	public void setNivelBateria(int nivelBateria) {
+		this.nivelBateria = nivelBateria;
+	}
+
+
+	@Override 
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("Bateria restante " + nivelBateria + " %");
+	}
+
+	@Override
+	public void acelerar(int valor) {
+		 super.acelerar(valor);
+		 
+		int consulmoBateria = valor * CONSUMO_POR_ACELERACAO;
+		nivelBateria -= consulmoBateria;
+		 
+	}
 }
